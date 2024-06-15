a060a1f15c478c5b4d1ff96c83db8bb5317c12aa
Bonifacio <mikaelbonifacio@gmail.com>
2024-06-15 00:06:08 -0300

    Add long, int, char

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,15 @@ public class Main {
         System.out.println("Hello World!");
 
 
-        byte variavelByte = 120;
-        System.out.println(variavelByte);
+        byte variavelByte = -127;
+        //short -32768 a 32767
+        //char 0 a 65535
+        //int 4 bytes (-2b a 2b)
+        // long -9t a 9t
+
+        int variavelInt =  2147483;
+
+
+        System.out.println(variavelInt);
     }
 }
