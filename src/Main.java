6a37b0a424ad5acd5f70db50f8f83939ad49d312
Bonifacio <mikaelbonifacio@gmail.com>
2024-06-15 21:53:02 -0300

    Garbage collector example

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Cachorro cachorro1 = new Cachorro("zayla","Neutro","Marron",25,7,5.5);
+        Cachorro cachorro1 = new Cachorro("Rex","Neutro","Marron",25,7,5.5);
+        Cachorro cachorro2 = new Cachorro("Rex","Neutro","Marron",25,7,5.5);
+
         /*
-        cachorro1.setNome("zayla");
-        cachorro1.setCor("Marron");
-        cachorro1.setAltura(25);
-        cachorro1.setPeso(5.5);
-        cachorro1.setTamanhoDoRabo(7);
+        System.out.println(cachorro1.toString());
+        System.out.println(cachorro2.toString());
         */
 
-        System.out.println(cachorro1.getNome());
-        System.out.println(cachorro1.getPeso());
-        //System.out.println(cachorro1);
-        cachorro1.latir();
-        System.out.println("O cachorro pegou uma "+ cachorro1.pegar());
+        cachorro2 = cachorro1;
 
-        System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
-        System.out.println("O cachorro está " + cachorro1.interagir("nada"));
-        System.out.println("O cachorro está " + cachorro1.interagir("vai dormir"));
-
+        System.out.println(cachorro1.toString());
+        System.out.println(cachorro2.toString());
     }
 }
